e422704542973e37d0992b0a1afcca85ea3463b7
innfi <innfi.world@gmail.com>
2020-03-10 22:49:36 +0900

    react initial

--- SampleNodeJs/server.js
+++ SampleNodeJs/server.js
@@ -1,8 +1,15 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var staticPath = path.join(__dirname, '/');
+app.use(express.static(staticPath));
+
+app.set('port', 1337);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('listening');
+});
