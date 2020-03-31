7dfaecf3347be4b0ff846096321d80f156a69ab8
innfi <innfi.world@gmail.com>
2020-03-31 21:48:41 +0900

    mysql update: insert/select

--- NodeMySql/app.js
+++ NodeMySql/app.js
@@ -16,12 +16,28 @@ conn.connect(function (err) {
     //    console.log("database created");
     //});
 
-    var createQeury = "create table Players (UserId INT(11), Nickname VARCHAR(255))";
+    //var createQuery = "create table Players (UserId INT(11), Nickname VARCHAR(255))";
 
-    conn.query(createQeury, function (err, result) {
+    //conn.query(createQuery, function (err, result) {
+    //    if (err) throw err;
+
+    //    console.log("table created");
+    //});
+
+    var insertQuery = "insert into Players(UserId, Nickname) values(1, \"innfi\")";
+
+    conn.query(insertQuery, function (err, result) {
         if (err) throw err;
 
-        console.log("table created");
+        console.log("insert finished");
+    });
+
+    var selectQuery = "select * from Players";
+
+    conn.query(selectQuery, function (err, result, fields) {
+        if (err) throw err;
+
+        console.log(result);
     });
 });
 
