b4525d7b2646656385c9db434a83a21a64c0909e
innfi <innfi.world@gmail.com>
2020-04-10 21:47:25 +0900

    proxy update

--- React-ToDo/backend/routes/index.js
+++ React-ToDo/backend/routes/index.js
@@ -1,9 +1,28 @@
-import { Router } from 'express';
-var router = Router();
+const express = require('express');
+const mysql = require('mysql');
+
+
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-export default router;
+let conn = mysql.createConnection({
+  host: "localhost",
+  user: "innfi", 
+  password: "test_pass",
+  database: "InnfisTable"
+});
+
+router.post('/todo-history', (req, res) => {
+  res.send('test');
+  // conn.query("insert into todo_history(todo_text, complete) values ('hello', 1)", (err) => {
+  //   if(err) throw err;
+
+  //   res.send('test');
+  // });
+});
+
+module.exports = router;
