6d38ced74b264a3192cb0ab14574d3d2c5edcafd
innfi <innfi.world@gmail.com>
2020-04-13 22:19:30 +0900

    write todos to RDB

--- React-ToDo/backend/routes/todo-history.js
+++ React-ToDo/backend/routes/todo-history.js
@@ -1,11 +1,37 @@
 var express = require("express");
+var mysql = require('mysql');
 var router = express.Router();
+
+let conn = mysql.createConnection({
+    host: "localhost",
+    user: "innfi", 
+    password: "test_pass",
+    database: "InnfisTable"
+});
 
 router.get("/index", (req, res, next) => {
 	res.send("api is working");
 });
 
 router.post("/", (req, res, next) => {
+	conn.connect((err) => {
+		if(err) throw err;
+		console.log("Connected to DB");
+
+		for(var index in req.body.todos) {
+			//console.log("text: " + todo.text);
+			//console.log("isCompleted: " + todo.isCompleted);
+			var todo = req.body.todos[index];
+			console.log(todo.text);
+			console.log(todo.isCompleted);
+			
+			var sql = `insert into todo_history(todo_text, complete) values ("${todo.text}", ${Number(todo.isCompleted)})`;
+			conn.query(sql, (err, result) => {
+				if(err) throw err;
+			});
+		}		
+	});
+
 	res.send("test");
 });
 
