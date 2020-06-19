e903802406be6fa1152f9077396e582e680a5390
Innfi <innfi.world@gmail.com>
2020-06-19 21:03:40 +0900

    unit test template

---
+++ React-ToDo/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+
+describe('test template', () => {
+    it('call expect', () => {
+        expect(true).toBeTruthy();
+    });
+});
