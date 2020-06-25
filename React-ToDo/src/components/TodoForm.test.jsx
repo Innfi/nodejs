0a288dda3f1a0abcd8ff2bd538fc1485fcb1f3b7
innfi <innfi.world@gmail.com>
2020-06-25 23:42:14 +0900

    adding unit tests

--- React-ToDo/src/components/TodoForm.test.jsx
+++ React-ToDo/src/components/TodoForm.test.jsx
@@ -4,6 +4,16 @@ import TodoForm from './TodoForm';
 
 describe ('TodoForm Component', () => {
     it('TodoForm has a label', () => {
-        
+        const { getByText } = render(<TodoForm />);
+
+        var labelText = getByText('Add new todo!');
+        expect(labelText).toBeTruthy();
     });
+    /*
+    it('handleChange is called', () => {
+        const handleChange = jest.fn();
+        const methodProps = { handleChange };
+        const result = render(<TodoForm  {...methodProps} />);
+    });
+    */
 });
