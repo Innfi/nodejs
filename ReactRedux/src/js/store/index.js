e6da4485b092d9e0b70afdaee5c17cc2a0b4f133
innfi <innfi.world@gmail.com>
2020-07-13 23:20:12 +0900

    react-redux tutorial: redux-toolkit

--- ReactRedux/src/js/store/index.js
+++ ReactRedux/src/js/store/index.js
@@ -1,22 +1,56 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducers/index';
-import { forbiddenWordsMiddleware } from '../middleware';
-import createSagaMiddleware from 'redux-saga';
-import apiSaga from '../sagas/api-saga';
+//import { createStore, applyMiddleware, compose } from 'redux';
+//import rootReducer from '../reducers/index';
+//import { forbiddenWordsMiddleware } from '../middleware';
+//import createSagaMiddleware from 'redux-saga';
+//import apiSaga from '../sagas/api-saga';
+//
+//
+//const initializeSagaMiddleware = createSagaMiddleware();
+//
+//const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//
+//const store = createStore(
+//    rootReducer,
+//    storeEnhancers(
+//        applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
+//    )
+//);
+//
+//initializeSagaMiddleware.run(apiSaga);
+//
+//
+//export default store;
 
+import { configureStore, getDefaultMiddleware, createSlice } from '@reduxjs/toolkit';
 
-const initializeSagaMiddleware = createSagaMiddleware();
+const middleware = [
+    ...getDefaultMiddleware(),
+];
 
-const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const authState = {
+    token: "",
+    error: "" 
+};
 
-const store = createStore(
-    rootReducer,
-    storeEnhancers(
-        applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
-    )
-);
+const authSlice = createSlice({
+    name: "auth",
+    initialState: authState,
+    reducers: {
+        loginSuccess: (state, action) => {
+            state.token = action.payload;
+        },
+        loginFailed: (state, action) => {
+            state.error = action.payload;
+        }
+    }
+});
 
-initializeSagaMiddleware.run(apiSaga);
+const { loginSuccess, loginFailed } = authSlice.actions;
+const authReducer = authSlice.reducer;
 
-
-export default store;
+const store = configureStore({
+    reducer: {
+        auth: authReducer,
+    }, 
+    middleware,
+});
