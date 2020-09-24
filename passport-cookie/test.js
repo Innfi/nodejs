f0b9bd6bef927c38514d20474b8076431d50bd21
innfi <innfi.world@gmail.com>
2020-09-25 08:20:18 +0900

    supertest api exercise

--- passport-cookie/test.js
+++ passport-cookie/test.js
@@ -1,16 +1,35 @@
 const assert = require('assert');
-const requst = require('supertest');
+const request = require('supertest');
 const express = require('express');
-const { request } = require('http');
 
 
 const app = express();
 app.get('/user', (req, res) => res.send('test'));
 
-describe('api test', () => {
-    it('dummy app', () => {
+describe('supertest', () => {
+    it('dummy app', (done) => {
         request(app)
         .get('/user')
-        .expect(200);
+        .expect(200)
+        .end(() => {
+            done();
+        });
     });
+
+    it('invalid path', (done) => {
+        request(app)
+        .get('/notexist')
+        .expect(404)
+        .end((err, result) => {
+            if(err) {
+                console.log('err: ', err);
+            } 
+
+            done();
+        });
+    });
+
+    //it('post data', () => {
+
+    //});
 });
