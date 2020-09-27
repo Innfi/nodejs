6b35dc3307541743e32e0e304db2b0d7946c62e8
innfi <innfi.world@gmail.com>
2020-09-27 23:28:33 +0900

    supertest: post

--- passport-cookie/test/run-supertest.js
+++ passport-cookie/test/run-supertest.js
@@ -1,10 +1,18 @@
-const assert = require('assert');
 const request = require('supertest');
 const express = require('express');
+const assert = require('assert');
 
 
 const app = express();
+app.use(express.json());
 app.get('/user', (req, res) => res.send('test'));
+app.post('/user', (req, res) => {
+    if(req.body.name !== 'innfi') res.send(400);
+
+    res.send({
+        email: 'innfi@test.com'
+    });
+});
 
 describe('supertest', () => {
     it('dummy app', (done) => {
@@ -29,7 +37,14 @@ describe('supertest', () => {
         });
     });
 
-    //it('post data', () => {
-
-    //});
+    it('post data', (done) => {
+        request(app)
+        .post('/user')
+        .send({name: 'innfi'})
+        .expect(200)
+        .end((err, res) => {
+            assert.strictEqual(res.body.email, 'innfi@test.com');
+            done();
+        });
+    });
 });
