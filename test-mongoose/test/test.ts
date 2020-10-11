d0112a9c5fc0edea8be273f9337c5889f2b0272c
innfi <innfi.world@gmail.com>
2020-10-11 17:18:54 +0900

    test-mongoose: initial commit

---
+++ test-mongoose/test/test.ts
@@ -0,0 +1,19 @@
+import assert from "assert";
+import mongoose from 'mongoose';
+
+
+describe('Mongoose', () => {
+    const dbpath = 'mongodb://127.0.0.1/my_database';
+
+    it('test connect', () => {
+        mongoose.connect(dbpath, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then((result) => {
+            console.log('connections ', result.connection.collections);
+        });
+
+        const db = mongoose.connection;
+        db.on('err', () => {
+            console.log('connection error');
+        });
+    });
+});
