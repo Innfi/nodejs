9c22ca2ecdd347511d231e8bfd7f48a298995b82
innfi <innfi.world@gmail.com>
2020-10-21 08:27:38 +0900

    filling out Register component

--- test-mongoose/frontend/src/components/auth/Register.tsx
+++ test-mongoose/frontend/src/components/auth/Register.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, MouseEvent, ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
 
@@ -22,6 +22,78 @@ class Register extends Component<RegisterState> {
     onChange = (e: any) => {
         this.setState({ [e.target.id]: e.target.value });
     };
+
+    onSubmit = (e: any) => { 
+        e.preventDefault();
+
+        const newUser: object = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+
+        console.log(newUser);
+    }
+
+    render(): ReactNode {
+        const { errors } = this.state;
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">keyboard_backspace</i>
+                            back to home
+                        </Link>
+                        <div className="col s12" style={{ paddingLeft: "11.250.px"}}>
+                            <h4>
+                                <b>Register</b> below
+                            </h4>
+                            <p className="grey-text text-darken-1">
+                                Already have an account? <Link to="/login">Login</Link>
+                            </p>
+                        </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <input onChange={this.onChange} 
+                                        value={this.state.name} 
+                                        error={errors.name} 
+                                        id="name" 
+                                        type="text" />
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input onChange={this.onChange} 
+                                        value={this.state.email} 
+                                        error={errors.email} 
+                                        id="email" 
+                                        type="email" />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input onChange={this.onChange} 
+                                        value={this.state.password} 
+                                        error={errors.password} 
+                                        id="password" 
+                                        type="password" />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                            <div className="input-field col s12">
+                                <input onChange={this.onChange} 
+                                        value={this.state.password2} 
+                                        error={errors.password2} 
+                                        id="password2" 
+                                        type="password" />
+                                <label htmlFor="password">Confirm Password</label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Register;
