62c0f9c4a5557bad1be8f58528546f98c40b24df
innfi <innfi.world@gmail.com>
2020-10-23 08:21:34 +0900

    filling out Login component

--- test-mongoose/frontend/src/components/auth/Login.tsx
+++ test-mongoose/frontend/src/components/auth/Login.tsx
@@ -19,7 +19,7 @@ class Login extends Component<LoginState> {
         this.setState({ [e.target.id]: e.target.value });
     };
 
-    onSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+    onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         const userData = {
@@ -36,7 +36,29 @@ class Login extends Component<LoginState> {
         return (
             <div className="container">
                 <div style={{ marginTop: "4rem" }} className="row">
-
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">keyboard_backspace</i>
+                            Back to home
+                        </Link>
+                        <div className="col s12" style={{ paddingLeft: "11.250px"}}>
+                            <h4>
+                                <b>Login</b> below
+                            </h4>
+                            <p className="grey-text text-darken-1">
+                                <Link to="/register">Register</Link>
+                            </p>
+                        </div>
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <input onChange={this.onChange} 
+                                        value={this.state.email} 
+                                        error={errors.email}
+                                        id="email" 
+                                        type="email" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
