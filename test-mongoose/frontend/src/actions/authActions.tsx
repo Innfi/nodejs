0c329885158e04d73685bcc038a4e954fe267793
innfi <innfi.world@gmail.com>
2020-10-28 08:23:51 +0900

    auth actions

--- test-mongoose/frontend/src/actions/authActions.tsx
+++ test-mongoose/frontend/src/actions/authActions.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Dispatch } from 'redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from '../utils/setAuthToken';
@@ -13,7 +13,7 @@ import {
 export const registerUser = (userData: object, history: any) => (dispatch: Dispatch) => {
     axios
     .post('/api/users/register', userData)
-    .then(res => history.push('/login'))
+    .then((res: AxiosResponse) => history.push('/login'))
     .catch((err) => {
         dispatch({
             type: GET_ERRORS,
@@ -21,3 +21,28 @@ export const registerUser = (userData: object, history: any) => (dispatch: Dispa
         });
     });
 };
+
+export const loginUser = (userData: object) => (dispatch: Dispatch) => {
+    axios.post('/api/users/login', userData)
+    .then((res: AxiosResponse) => {
+        const { token } = res.data;
+        localStorage.setItem("jwtToken", token);
+        setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(setCurrentUser(decoded));
+    })
+    .catch((err: any) => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }));
+}
+
+export const setCurrentUser = (decoded: unknown) => {
+    return { type: USER_LOADING };
+};
+
+export const logoutUser = () => (dispatch: Dispatch) => {
+    localStorage.removeItem("jwtToken");
+    setAuthToken(''); //FIXME
+    dispatch(setCurrentUser({}));
+}
--- test-mongoose/frontend/src/store.tsx
+++ test-mongoose/frontend/src/store.tsx
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
@@ -11,7 +12,8 @@ const middleware = [thunk];
 //}
 
 const store = createStore(
-    () => [],
+    rootReducer,
+    initialState,
     compose(
         applyMiddleware(...middleware), 
         (window as any).__REDUX_DEVTOOLS_EXTENSION__ && 
