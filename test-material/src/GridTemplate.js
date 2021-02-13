d3f8977b5f6b86e0a028acf69cb100aa45f8601b
innfi <innfi.world@gmail.com>
2021-02-13 10:40:40 +0900

    test-material: working on with grids

--- test-material/src/GridTemplate.js
+++ test-material/src/GridTemplate.js
@@ -4,12 +4,33 @@ import { Grid } from '@material-ui/core'
 
 
 export default function GridTemplate() {
+    function Rows() {
+        return (
+            <React.Fragment>
+                <Grid item xs={4}>
+                    <MediaCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <MediaCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <MediaCard />
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
+
     return (
-        <Grid container spacing={1} direction="column">
-            <Grid item xs={12} justify="center">
-                <MediaCard />
-                <MediaCard />
-                <MediaCard />
+        <Grid container spacing={1} direction="row" >
+            <Grid container item xs={12} justify="center">
+                <Rows />
+            </Grid>
+            <Grid container item xs={12} justify="center">
+                <Rows />
+            </Grid>
+            <Grid container item xs={12} justify="center">
+                <Rows />
             </Grid>
         </Grid>
     )
