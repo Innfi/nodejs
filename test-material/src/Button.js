54612a386d295c01a56e87ae9590f20e2e9a87d3
innfi <innfi.world@gmail.com>
2021-02-20 21:50:24 +0900

    test-material: buttons

---
+++ test-material/src/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+
+
+const useStyles = makeStyles((theme) => ({
+    button: {margin: theme.spacing(1)}
+}));
+
+export default function IconLabelButtons() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Button variant="contained" color="secondary" className={classes.button} 
+                startIcon={<DeleteIcon />}> delete </Button>
+            <Button variant="contained" color="primary" className={classes.button}
+                endIcon={<Icon>send</Icon>}>send</Button>
+            <Button variant="contained" color="default" className={classes.button} 
+                startIcon={<CloudUploadIcon />}>upload</Button>
+        </div>
+    );
+};
