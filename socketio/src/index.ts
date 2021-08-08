739a525240c5b1e8c1e037595745911b5e8257b2
innfi <innfi.world@gmail.com>
2021-08-08 17:37:02 +0900

    socketio: delayed register event

--- socketio/src/index.ts
+++ socketio/src/index.ts
@@ -1,28 +1,85 @@
+import 'reflect-metadata'
+import { Container, Service } from 'typedi';
 import express, { Request, Response } from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
-const app = express();
-const httpServer = http.createServer(app);
-const io = new Server(httpServer);
 
-app.get('/', (req: Request, res: Response) => {
-	res.sendFile(__dirname + '/index.html');
-});
+type MsgHandlerDict = { [id: string]: Function };
 
-io.on('connection', (socket) => {
-	console.log('client connected');
-	socket.on('chat message', (msg) => {
-		io.emit('chat message', msg);
-	});
-	// console.log('connection received');
-	// socket.send({
-	// 	err: 'ok', 
-	// 	msg: 'test message from server'
-	// });
-});
+@Service()
+class SocketIoRunner {
+	protected app: any 
+	protected httpServer: http.Server;
+	protected io: Server;
+	protected port: number = 3000;
+	protected handlerDict: MsgHandlerDict = {};
 
-httpServer.listen(3000, () => {
-	console.log('start listening');
-});
+	public constructor() {
+		this.app = express();
+		this.httpServer = http.createServer(this.app);
+		this.io = new Server(this.httpServer);
+
+		this.registerIndexPage();
+	}
+
+	protected registerIndexPage(): void {
+		this.app.get('/', (req: Request, res: Response) => {
+			res.sendFile(__dirname + '/index.html');
+		});
+	} 
+
+	public registerConnected(): void {
+		this.io.on('connection', (socket) => {
+			console.log('client connected');
+			const keys: string[] = Object.keys(this.handlerDict);
+
+			for(const key of keys) {
+				socket.on(key, (msg) => {
+					const handler: Function = this.handlerDict[key];
+					const result = handler.call(this, msg);
+					console.log(`result: ${JSON.stringify(result)}`);
+
+					//this.io.emit(result.keyType, result.msg);
+					socket.emit(result.keyType, result.msg);
+				});
+			}
+		});
+	}
+
+	public registerEvent(eventName: string, handler: Function): void {
+		this.handlerDict[eventName] = handler;
+	}
+
+	public listen(): void {
+		this.httpServer.listen(this.port, () => {
+			console.log('start listening');
+		});
+	}
+};
+
+@Service()
+class ChatMsgHandler {
+	public static chatMsgKey: string = 'msgTypeChat';
+
+	public constructor() {}
+
+	public handleChatMsg(...args: any[]): any {
+		const receivedMsg: string = args[0].text;
+
+		return {
+			err: 'ok',
+			keyType: ChatMsgHandler.chatMsgKey,
+			msg: receivedMsg
+		};
+	}
+}
+
+const runner = Container.get(SocketIoRunner);
+const chatHandler = Container.get(ChatMsgHandler);
+
+runner.registerEvent(ChatMsgHandler.chatMsgKey, chatHandler.handleChatMsg);
+
+runner.registerConnected();
+runner.listen();
 
