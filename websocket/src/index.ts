8741140cef88efa4b27f501483cdf01cba7c43db
innfi <innfi.world@gmail.com>
2021-08-20 18:39:57 +0900

    websocket: extending websocket class

--- websocket/src/index.ts
+++ websocket/src/index.ts
@@ -1,21 +1,58 @@
 import ws from 'ws';
 import http from 'http';
+import { v4 } from 'uuid';
 
 
-const webServer: http.Server = http.createServer();
+class ExWebSocket extends ws {
+    public id: string;
 
-const wsServer = new ws.Server({ server: webServer });
-wsServer.on('connection', (socket: ws) => {
-    console.log('client connected');
+};
 
-    socket.on('message', (message: string) => {
-        console.log(`received: ${message}`);
+class WsService {
+    protected webServer: http.Server;
+    protected wsServer: ws.Server;
+    protected socketIds: Set<string>;
 
-        socket.send(`reply: ${message}`);
-    });
-});
+    public constructor() {
+        this.init();
+        this.registerEvents();
+    }
+
+    protected init(): void {
+        this.webServer = http.createServer();
+        this.wsServer = new ws.Server({ server: this.webServer });
+        this.socketIds = new Set<string>();
+    }
+
+    protected registerEvents(): void {
+        this.wsServer.on('connection', (socket: ExWebSocket) => {
+            console.log('client connected');
+            const newSocketId: string = v4();
+            if(this.socketIds.has(newSocketId)) {
+                console.log('socketId duplicate. what to do?')
+            }
+
+            this.socketIds.add(newSocketId);
+            socket.id = newSocketId;
+            
+            console.log(`socket size: ${this.socketIds.size}`);
+
+            socket.on('message', (message: string) => {
+                console.log(`received: ${message}`);
+        
+                socket.send(`reply: ${message}`);
+            });
+        });
+    }
+
+    public startListen(): void {
+        this.webServer.listen(3000, () => {
+            console.log('start listening');
+        });
+    }
+};
 
 
-webServer.listen(3000, () => {
-    console.log('start listening');
-});
+const wsService = new WsService();
+
+wsService.startListen();
