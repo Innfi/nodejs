da30097158a2f5f642d4a23d17894d67c8f48ee6
innfi <innfi.world@gmail.com>
2021-11-19 16:29:41 +0900

    chart-react-ts: testing

--- chart-react-ts/src/candlestick-d3/CandlestickD3.tsx
+++ chart-react-ts/src/candlestick-d3/CandlestickD3.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import d3 from 'd3';
+import classNames from 'classnames';
 
 
 interface StockUnit {
@@ -9,6 +10,33 @@ interface StockUnit {
   low: number;
   close: number;
   volume: number;
+};
+
+interface CandleProp {
+  data: StockUnit;
+  x: number;
+  candleWidth: number, 
+  pixelFor: Function;
+};
+
+interface ChartDims {
+  pixelWidth: number;
+  pixelHeight: number;
+  dollarHigh: number;
+  dollarLow: number;
+  dollarDelta: number;
+};
+
+interface CrosshairProp {
+  x: number;
+  y: number;
+  chartDims: ChartDims;
+};
+
+interface ChartProp {
+  data: StockUnit[], 
+  chartWidth: number;
+  chartHeight: number;
 };
 
 const randomOne = (weight = 1) => {
@@ -44,21 +72,122 @@ const generateData = (): StockUnit[] => {
   });
 };
 
-interface CandleProp {
-  data: StockUnit;
-  x: number;
-  candleWidth: number, 
-  pixelFor: Function;
-};
+const Crosshair = (props: CrosshairProp): JSX.Element => {
+  const { x, y, chartDims } = props;
 
-const Candle = (props: CandleProp) => {
-  const { data, x, candleWidth, pixelFor } = props;
-
-  const up = data.close > data.open;
+  if(x+y === 0) return (<></>);
 
   return (
     <>
-      
+      <line x1={0} y1={y} x2={chartDims.pixelWidth} y2={y} 
+        className={classNames({ crosshair: true, horz: true })} />
+      <line x1={x} y1={0} x2={x} y2={chartDims.pixelHeight} 
+        className={classNames({ crosshair: true, vert: true })} />
     </>
   );
 };
+
+const Candle = (props: CandleProp): JSX.Element => {
+  const { data, x, candleWidth, pixelFor } = props;
+
+  const up = data.close > data.open;
+  const barTop = pixelFor(up ? data.close : data.open );
+  const barBottom = pixelFor(up ? data.open : data.close );
+  const barHeight = barBottom - barTop;
+  const wickTop = pixelFor(data.high);
+  const wickBottom = pixelFor(data.low);
+
+  return (
+    <>
+      <rect x={x -candleWidth / 2 } y={barTop} width={candleWidth} 
+        height={barHeight} 
+        className={classNames({ candle: true, up: up, down: !up })} />
+      <line x1={x} y1={barTop} x2={x} y2={wickTop} 
+        className={classNames({ wick: true, top: true, up: up, down: !up })} />
+      <line x1={x} y1={barBottom} x2={x} y2={wickBottom} 
+        className={classNames({ wick: true, bottom: true, up: up, down: !up })} />
+    </>
+  );
+};
+
+const Chart = (props: ChartProp): JSX.Element => {
+  const { data, chartWidth, chartHeight } = props;
+
+  const [mousePos, setMousePos] = useState({
+    x: 0, y: 0
+  });
+
+  const dollarHigh = d3.max(data.map(bar => bar.high))! * 1.05;
+  const dollarLow = d3.min(data.map(bar => bar.low))! * 0.95;
+  const chartDims: ChartDims = {
+    pixelWidth: chartWidth, 
+    pixelHeight: chartHeight, 
+    dollarHigh: dollarHigh, 
+    dollarLow: dollarLow, 
+    dollarDelta: dollarHigh - dollarLow
+  };
+
+  const dollarAt = (pixel: number): string => {
+    const dollar = (Math.abs(pixel - chartDims.pixelHeight) / 
+      chartDims.pixelHeight) * chartDims.dollarDelta + chartDims.dollarLow;
+
+    return pixel > 0 ? dollar.toFixed(2) : '-';
+  };
+
+  const pixelFor = (dollar: number): number => {
+    return Math.abs(
+      ( ((dollar - chartDims.dollarLow) / chartDims.dollarDelta) * 
+        chartDims.pixelHeight) - chartDims.pixelHeight
+    );
+  };
+
+  const onMouseLeave = () => { setMousePos({ x: 0, y: 0}); };
+  const onMouseInside = (e: any) => {
+    setMousePos({
+      x: e.nativeEvent.x - Math.round(e.currentTarget.getBoundingClientRect.left), 
+      y: e.nativeEvent.y - Math.round(e.currentTarget.getBoundingClientRect.top), 
+    });
+  };
+
+  const candleWidth = Math.floor((chartWidth / data.length) * 0.7);
+
+  return (
+    <svg width={chartWidth} height={chartHeight} className="chart" 
+      onMouseMove={onMouseInside} onMouseLeave={onMouseLeave}>
+    { data.map((bar: StockUnit, index: number) => {
+      const candleX = (chartWidth / (data.length+1) * (index+1));
+      return (
+        <Candle key={index} data={bar} x={candleX} candleWidth={candleWidth} 
+          pixelFor={pixelFor} />
+      );
+    }) }
+    <text x="10" y="16" fill="white" fontSize="10">
+      <tspan>
+        Mouse: {mousePos.x}, {mousePos.y}
+      </tspan>
+      <tspan x="10" y="30">
+        Dollars: ${dollarAt(mousePos.y)}
+      </tspan>
+    </text>
+    <Crosshair x={mousePos.x} y={mousePos.y} chartDims={chartDims} />
+    </svg>
+  );
+};
+
+
+const CandlestickChartD3 = (): JSX.Element => {
+  const chartWidth = 500;
+  const chartHeight = 300;
+
+  const [data, setData] = useState(generateData());
+
+  return (
+    <div>
+      <div className="content">
+        <Chart data={data} chartWidth={chartWidth} chartHeight={chartHeight} />
+      </div>
+    </div>
+  );
+};
+
+export default CandlestickChartD3;
--- chart-react-ts/src/index.tsx
+++ chart-react-ts/src/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-//import LineChart from './component/Line';
-//import StackedBar from './component/Bar';
-import TestChart from './component/Candlesticks';
+import CandlestickD3 from './candlestick-d3/CandlestickD3';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TestChart />
+    <CandlestickD3 />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
