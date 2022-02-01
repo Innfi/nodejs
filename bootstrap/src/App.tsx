471e08b44348eacae06cb719b8d08b941e79a6cb
innfi <innfi.world@gmail.com>
2022-02-01 17:59:12 +0900

    react-bootstrap: tried out some components

--- bootstrap/src/App.tsx
+++ bootstrap/src/App.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-import { Button, Col, Row } from 'react-bootstrap';
+import { Button, Row, Table } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <Row className="mx-0">
-      <Button as={Col} variant="primary">Button #1</Button>
-    </Row>
     <div>
-      start from here
+      <Row className="mx-0">
+        <Button variant="primary">Button #1</Button>
+      </Row>
+      <div>
+        start from here
+      </div>
+      <Table striped={true} bordered={true} hover={true}>
+        <thead>
+          <th>#</th>
+          <th>first name</th>
+          <th>last name</th>
+          <th>username</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>mark</td>
+            <td>otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>jacob</td>
+            <td>thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>larry</td>
+            <td>@twtr</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 };
