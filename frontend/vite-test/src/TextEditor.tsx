cb67b6a5c89201bac61708a3315a233dbcfaa4e0
innfi <innfi.world@gmail.com>
2024-01-25 22:47:27 +0900

    vite-test: how to add a new icon to ReactQuill?

--- frontend/vite-test/src/TextEditor.tsx
+++ frontend/vite-test/src/TextEditor.tsx
@@ -1,23 +1,45 @@
 import { Typography } from "@mui/material";
-import { useMemo } from "react";
-import ReactQuill from "react-quill";
+import { useMemo, useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+
+let icons = ReactQuill.Quill.import('ui/icons');
+icons['table'] = '<i class="fa fa-link" aria-hidden="true"></i>';
+Quill.register(icons, true);
 
 export const QuillEditor = () => {
+  const [textData, setTextData] = useState("");
+
   const modules = useMemo(() => ({
     toolbar: {
       container: [
-        ["bold", "italic", "underline"]
+        ["bold", "italic", "underline"],
+        ["image", "table"]
       ]
-    }
+    },
   }), []);
+
+  const onChange = (e: string) => {
+    setTextData(e);
+    console.log(`text: ${textData}`);
+  };
 
   return (
     <div>
-      <Typography>start from here</Typography>
-      <ReactQuill 
-        theme="snow"
-        modules={modules}
-      />
+      <div>
+        <Typography>start from here</Typography>
+        <ReactQuill 
+          theme="snow"
+          modules={modules}
+          onChange={(value: string) => onChange(value)}
+        />
+      </div>
+      <div>
+        <h3>title here</h3>
+        <div dangerouslySetInnerHTML={{
+          __html: textData
+        }} />
+      </div>
     </div>
   );
 };
