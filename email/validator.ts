efe933bebdba76dec7e365730db433a7ce8a6d86
innfi <innfi.world@gmail.com>
2025-03-13 22:56:21 +0900

    email: tried smtp-protocol but it seems too old

--- email/validator.ts
+++ email/validator.ts
@@ -1,10 +1,35 @@
 import { query } from 'dns-query';
+import smtp from 'smtp-protocol';
 
-const queryMxRecord = async (domain: string, endpoints?: string[]) => {
+type MXQueryResponseCode = 'NOERROR' | 'NXDOMAIN';
+
+const queryMxRecord = async (domain: string, endpoints?: string[]): Promise<MXQueryResponseCode> => {
   const { answers, rcode } = await query(
     { question: { type: 'MX', name: domain } },
     { endpoints: endpoints ?? ['8.8.8.8'] }
   );
+
+  return rcode as MXQueryResponseCode;
 };
 
+// const verifyLocalname = async (email: string): Promise<void> => {
+//   const [localname, domain] = email.split('@');
+// 
+//   const client = smtp.connect(25, domain);
+// };
 
+const validateEmail = async (email: string): Promise<void> => {
+  console.log(`email: ${email}`);
+
+  const mxRecordResponse = await queryMxRecord(email.split('@')[1]);
+  if (mxRecordResponse !== 'NOERROR') {
+    console.log(`${email}] invalid domain`);
+    return;
+  }
+
+  // await verifyLocalname(email);
+};
+
+(async () => {
+  await validateEmail('thisis@gmail.com');
+})()
