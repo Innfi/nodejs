e66b152e7c6dca822441ea3bcc94256088cf8455
innfi <innfi.world@gmail.com>
2025-04-16 23:34:08 +0900

    frontend: drag and drop using mrt

---
+++ frontend/vite-dnd/src/App.tsx
@@ -0,0 +1,95 @@
+import { useMemo, useState } from 'react';
+import {
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+  type MRT_Row,
+  MRT_TableContainer,
+} from 'material-react-table';
+
+export type Person = {
+  firstName: string;
+  lastName: string;
+  city: string;
+};
+
+export const initData = [
+  {
+    firstName: 'Dylan',
+    lastName: 'Murray',
+    city: 'East Daphne',
+  },
+  {
+    firstName: 'Raquel',
+    lastName: 'Kohler',
+    city: 'Columbus',
+  },
+  {
+    firstName: 'Ervin',
+    lastName: 'Reinger',
+    city: 'South Linda',
+  },
+  {
+    firstName: 'Brittany',
+    lastName: 'McCullough',
+    city: 'Lincoln',
+  },
+  {
+    firstName: 'Branson',
+    lastName: 'Frami',
+    city: 'Charleston',
+  },
+];
+
+const Example = () => {
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+    //column definitions...
+    () => [
+      {
+        accessorKey: 'firstName',
+        header: 'First Name',
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+      },
+    ],
+    [],
+    //end
+  );
+
+  const [data, setData] = useState(() => initData);
+
+  const table = useMaterialReactTable({
+    autoResetPageIndex: false,
+    columns,
+    data,
+    enableRowOrdering: true,
+    enableRowNumbers: true,
+    rowNumberDisplayMode: 'static',
+    enableSorting: false,
+    muiRowDragHandleProps: ({ table }) => ({
+      onDragEnd: () => {
+        const { draggingRow, hoveredRow } = table.getState();
+        if (hoveredRow && draggingRow) {
+          data.splice(
+            (hoveredRow as MRT_Row<Person>).index,
+            0,
+            data.splice(draggingRow.index, 1)[0],
+          );
+          data.forEach((v, index: number) => {
+            console.log(`index: ${index}, data: ${v.firstName}`);
+          });
+          setData([...data]);
+        }
+      },
+    }),
+  });
+
+  return <MRT_TableContainer table={table} />;
+};
+
+export default Example;
