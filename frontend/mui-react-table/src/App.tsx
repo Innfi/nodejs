e7e0403dcefae80767c32fa751d28ff59d4516ee
innfi <innfi.world@gmail.com>
2025-04-17 22:19:05 +0900

    frontend: datafetching in mrt

--- frontend/mui-react-table/src/App.tsx
+++ frontend/mui-react-table/src/App.tsx
@@ -5,6 +5,9 @@ import {
   useMaterialReactTable,
   type MRT_ColumnDef,
 } from 'material-react-table';
+import { useQuery } from 'react-query';
+import { IconButton, Tooltip } from '@mui/material';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 //example data type
 type Person = {
@@ -15,6 +18,11 @@ type Person = {
   address: string;
   city: string;
   state: string;
+};
+
+type FetchPersonList = {
+  list: Person[];
+  total: number;
 };
 
 //nested data is ok, see accessorKeys in ColumnDef below
@@ -67,6 +75,19 @@ const data: Person[] = [
 ];
 
 export function App() {
+
+  const { isError, isRefetching, isLoading, refetch } = useQuery<FetchPersonList>({
+    queryKey: 'PersonList',
+    queryFn: async () => {
+      // fetching: fetch / axios?
+      return {
+        list: [],
+        total: 0
+      } as FetchPersonList;
+    },
+  });
+
+
   //should be memoized or stable
   const columns = useMemo<MRT_ColumnDef<Person>[]>(
     () => [
@@ -113,6 +134,8 @@ export function App() {
     rowCount: dummyTotalRowCount,
     state: {
       pagination,
+      isLoading,
+      showProgressBars: isRefetching,
     },
     muiTableBodyRowProps: ({ row }) => ({
       onClick: (event) => {
@@ -122,6 +145,13 @@ export function App() {
         cursor: 'pointer',
       },
     }),
+    renderTopToolbarCustomActions: () => (
+      <Tooltip arrow title="refresh">
+        <IconButton onClick={() => refetch()}>
+          <RefreshIcon />
+        </IconButton>
+      </Tooltip>
+    ),
   });
 
   return <MaterialReactTable table={table} />;
