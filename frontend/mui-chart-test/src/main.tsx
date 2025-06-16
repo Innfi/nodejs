4b7d0dab1593ae8394e66d6d6c995d2791838782
innfi <innfi.world@gmail.com>
2025-06-16 22:54:44 +0900

    mui-chart-test: graph is good

--- frontend/mui-chart-test/src/main.tsx
+++ frontend/mui-chart-test/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx'
+import { CombinedChart } from './chart-quickstart.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CombinedChart />
   </StrictMode>,
 )
